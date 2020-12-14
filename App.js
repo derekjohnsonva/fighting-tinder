070097725bdeca07ebd07e59da556459d6b97994
derekjohnsonva <derekjohnsonva@gmail.com>
2020-12-14 14:34:18 -0500

    clone of repo

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Font, Icon } from 'expo'
+import { AppLoading} from 'expo-app-loading'
+import { Asset } from 'expo-asset'
+import React from 'react'
+import { StatusBar, StyleSheet, View } from 'react-native'
+import AppNavigator from './navigation/AppNavigator'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  }
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+          <StatusBar hidden />
+          <AppNavigator />
+        </View>
+      )
+    }
+  }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/splash.png'),
+        require('./assets/images/icon.png'),
+      ]),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Icon.MaterialIcons.font,
+        ...Icon.MaterialCommunityIcons.font,
+        ...Icon.FontAwesome.font,
+        ...Icon.Feather.font,
+      }),
+    ])
+  }
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error)
+  }
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true })
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
